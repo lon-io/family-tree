306ae283928905f08a3d98efea8f245c469cabe4
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-07-03 08:59:48 +0100

    Add deploy script

---
+++ bin/deploy-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+HEROKU_APP_NAME=lon-family-tree-server
+IMAGE_NAME=registry.heroku.com/${HEROKU_APP_NAME}/web
+
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
+docker build -t ${IMAGE_NAME} server/.
+docker push ${IMAGE_NAME}
+heroku container:release web --app ${HEROKU_APP_NAME}
